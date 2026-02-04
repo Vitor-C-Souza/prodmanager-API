f0d1bc1079c7b1f4091531828df34776938db5dd
Vitor-C-Souza <vitor_souza97@hotmail.com>
2026-02-04 20:12:43 -0300

    feat: configure dev/prod profiles and oracle database connection

---
+++ src/test/java/teste/autoflex/vitorcsouza/prodmanager/ProdManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package teste.autoflex.vitorcsouza.prodmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
