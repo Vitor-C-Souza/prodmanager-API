d3c84afcaa7cc29e5ba550a44688df36f16874f5
Vitor-C-Souza <vitor_souza97@hotmail.com>
2026-02-06 12:27:04 -0300

    refactor: removed CrudBase from ProductRawMaterial

--- src/main/java/teste/autoflex/vitorcsouza/prodmanager/domain/service/ProductRawMaterialService.java
+++ src/main/java/teste/autoflex/vitorcsouza/prodmanager/domain/service/ProductRawMaterialService.java
@@ -3,5 +3,8 @@ package teste.autoflex.vitorcsouza.prodmanager.domain.service;
 import teste.autoflex.vitorcsouza.prodmanager.domain.dto.ProductRawMaterialDTOReq;
 import teste.autoflex.vitorcsouza.prodmanager.domain.dto.ProductRawMaterialDTORes;
 
-public interface ProductRawMaterialService extends CrudBase<ProductRawMaterialDTORes, ProductRawMaterialDTOReq> {
+import java.util.UUID;
+
+public interface ProductRawMaterialService {
+    ProductRawMaterialDTORes link(UUID productId, ProductRawMaterialDTOReq productRawMaterialDTOReq);
 }
--- src/main/java/teste/autoflex/vitorcsouza/prodmanager/domain/service/impl/ProductRawMaterialServiceImpl.java
+++ src/main/java/teste/autoflex/vitorcsouza/prodmanager/domain/service/impl/ProductRawMaterialServiceImpl.java
@@ -14,7 +14,6 @@ import teste.autoflex.vitorcsouza.prodmanager.domain.repository.ProductRepositor
 import teste.autoflex.vitorcsouza.prodmanager.domain.repository.RawMaterialRepository;
 import teste.autoflex.vitorcsouza.prodmanager.domain.service.ProductRawMaterialService;
 
-import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -27,27 +26,14 @@ public class ProductRawMaterialServiceImpl implements ProductRawMaterialService 
 
     @Override
     @Transactional
-    public ProductRawMaterialDTORes save(ProductRawMaterialDTOReq dto) {
-        Product product = productRepository.findById(dto.productId()).orElseThrow(() -> new EntityNotFoundException("Product not found"));
-        RawMaterial rawMaterial = rawMaterialRepository.findById(dto.rawMaterialId()).orElseThrow(() -> new EntityNotFoundException("Raw Material not found"));
+    public ProductRawMaterialDTORes link(UUID productId, ProductRawMaterialDTOReq productRawMaterialDTOReq) {
+        Product product = productRepository.findById(productId).orElseThrow(() -> new EntityNotFoundException("Product not found"));
+        RawMaterial rawMaterial = rawMaterialRepository.findById(productRawMaterialDTOReq.rawMaterialId()).orElseThrow(() -> new EntityNotFoundException("Raw Material not found"));
 
-        ProductRawMaterial productRawMaterial = dto.toEntity(product, rawMaterial);
+        ProductRawMaterial productRawMaterial = productRawMaterialDTOReq.toEntity(product, rawMaterial);
+
+        productRawMaterialRepository.save(productRawMaterial);
 
         return ProductRawMaterialDTORes.fromEntity(productRawMaterial);
-    }
-
-    @Override
-    public List<ProductRawMaterialDTORes> findAll() {
-        return List.of();
-    }
-
-    @Override
-    public ProductRawMaterialDTORes findById(UUID id) {
-        return null;
-    }
-
-    @Override
-    public void deleteById(UUID id) {
-
     }
 }
