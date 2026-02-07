05125701ce60896bbd54d642b8259604ecac0646
Vitor-C-Souza <vitor_souza97@hotmail.com>
2026-02-07 13:17:06 -0300

    feat(model): implement User entity with UserDetails for security

---
+++ src/main/java/teste/autoflex/vitorcsouza/prodmanager/domain/model/Role.java
@@ -0,0 +1,6 @@
+package teste.autoflex.vitorcsouza.prodmanager.domain.model;
+
+public enum Role {
+    ADMIN,
+    USER
+}
---
+++ src/main/java/teste/autoflex/vitorcsouza/prodmanager/domain/model/User.java
@@ -0,0 +1,63 @@
+package teste.autoflex.vitorcsouza.prodmanager.domain.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@JsonIgnoreProperties({"authorities", "accountNonExpired", "accountNonLocked", "credentialsNonExpired", "enabled"})
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue
+    @Column(columnDefinition = "RAW(16)")
+    private UUID id;
+
+    private String username;
+    private String password;
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new org.springframework.security.core.authority.SimpleGrantedAuthority("ROLE_" + role.name()));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public String getUsername() {
+        return this.email;
+    }
+}
